3afc7af0c5684ad2ff7ec539e6879d52e99e5a1e
Kevin Oury <kev.oury@gmail.com>
2015-10-22 20:22:04 -0200

    Initial commit with project structure

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name webAppV2App
+ * @description
+ * # webAppV2App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('webAppV2App', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webAppV2App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the webAppV2App
+ */
+var app=angular.module('webAppV2App');
+
+app.controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
