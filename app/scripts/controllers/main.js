66b91e836915c1b93f6d882c8bd265b056aa3ca9
Kevin Oury <kev.oury@gmail.com>
2015-10-22 21:03:38 -0200

    Started implementing book info and book images page. Deteceted a bug in DB on livro.autor attribute

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,10 +9,22 @@
  */
 var app=angular.module('webAppV2App');
 
-app.controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+app.controller('bookCtrl', ['$scope', '$http', function($scope, $http) {
+    $http.get('http://localhost:1337/livro/56205dc6d45ee0cb103e80cc')
+      .then(function(response){
+        $scope.bookData=response.data;
+        $scope.debug=JSON.stringify(response.data);
+      },function(){$scope.bookTitle='testBookName';});
+    // Book id is hardcoded until now.
+
+
+    // Hardcoded for test presentation
+
+    // $scope.bookTitle= 'testBookName';
+    // $scope.bookEditor='testEditor';
+    // $scope.bookEdition='testEdition';
+    // $scope.bookYear='testYear';
+    // $scope.bookAuthor='testAuthor';
+    // $scope.bookImages=['http://www.femmeactuelle.fr/var/femmeactuelle/storage/images/cuisine/news-cuisine/vive-la-fete-du-pain-!-15199/11742777-1-fre-FR/vive-la-fete-du-pain.jpg',
+    //                    'http://imaginaire.passioncereales.fr/sites/default/files/field/image/PAINS.jpg'];
+}]);
